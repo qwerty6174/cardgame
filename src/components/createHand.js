b6f33e9d0ded532d78a92ec322f82c188e9f1c94
qwerty6174 <55049579+qwerty6174@users.noreply.github.com>
2020-06-11 14:32:39 +0300

    Create createHand.js

---
+++ src/components/createHand.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import createDeck from './cardDeck'
+
+
+function createHand(){
+    let deck = createDeck
+    let hand = []
+    for(let i=0;i<=6;i++){
+        let index = Math.random(36)
+        hand.push(deck[index])
+        console.log(hand)
+    }
+    return hand
+}
+
+export default createHand
